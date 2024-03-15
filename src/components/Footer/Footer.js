153bade7a0ee4c1dc44968c2c7e490a666709006
Mrunal Deo <mrunaldeo16@gmail.com>
2024-03-15 23:35:27 +0530

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">Blog |</a></li>
+        <li><a href="#">Scoring |</a></li>
+        <li><a href="#">Environment  |</a></li>
+        <li><a href="#">FAQ | </a></li>
+        <li><a href="#">About Us |</a></li>
+        <li><a href="#">Support |</a></li>
+        <li><a href="#">Careers |</a></li>
+        <li><a href="#">Terms Of Service |</a></li>
+        <li><a href="#">Privacy Policy </a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
