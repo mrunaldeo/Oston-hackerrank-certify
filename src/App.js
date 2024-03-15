4557c60aeee0aad86a23a5baf1f054c162200a42
Mrunal Deo <mrunaldeo16@gmail.com>
2024-03-15 23:29:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import Footer from './components/Footer/Footer';
+//import CertificationTests from './CertificationTests/CertificationTests';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
